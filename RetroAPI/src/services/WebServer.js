be2d25aec9d60a984910e1639045f927b59aed80
Juan M. Cardeso <a18juancg@iessanclemente.net>
2021-11-18 20:54:07 +0100

    Creada la primera ruta y el middleware de parseo del body

--- RetroAPI/src/services/WebServer.js
+++ RetroAPI/src/services/WebServer.js
@@ -29,13 +29,32 @@ class Server {
 
         this.express_logger.initializeLogger(this.app, fileConf, consoleConf);
         this.logger = this.express_logger.getLogger();
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas
+        this.routes();
+    }
+
+    middlewares(){
+        // Lectura y parseo del body de la request empleando json
+        this.app.use( express.json() );
+    }
+
+    routes(){
+        // Routes explícitas
+        this.app.get('/', (req, res) => {
+            res.status(202).json({msg:'RetroAPI, a free API to access retro hardware and software', status: true});
+            this.logger.debug("GET access from /");
+        });
     }
 
     start(){
         this.app.listen(
             this.port,
             () => {
-                this.logger.debug(`RetroAPI - Server running and listening on port ${this.port}`);
+                this.logger.info(`RetroAPI - Server running and listening on port ${this.port}`);
             }
         )
     }
