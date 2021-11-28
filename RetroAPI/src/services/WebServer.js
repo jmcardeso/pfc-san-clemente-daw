d816affa8083b850f34d0c7b6f4ad861574d35b1
Juan M. Cardeso <a18juancg@iessanclemente.net>
2021-11-28 14:03:49 +0100

    required por require (sic)

--- RetroAPI/src/services/WebServer.js
+++ RetroAPI/src/services/WebServer.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = required("cors");
+const cors = require("cors");
 
 const { dbConnectionSync } = require('./../configs/config_db');
 const { startLogger, logDebug, logInfo, logError }  = require('./../helpers/logger');
