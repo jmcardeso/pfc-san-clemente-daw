438a0e8e2fa83d1729fb6a48cbc1e81876136e20
Juan M. Cardeso <a18juancg@iessanclemente.net>
2021-12-08 22:11:51 +0100

    Empenzando con la búsqueda de dispositivos

--- web/assets/js/retroapi.js
+++ web/assets/js/retroapi.js
@@ -1,3 +1,34 @@
-const retroAjax = (filtro) => {
-
+const crearPeticionDispositivo = () => {
+    if ($('#txtNombre').val() != "") {
+        let url = 'devices?name=' + $('#txtNombre').val();
+        retroAjax(url);
+    } else alert("No hay nombre");  
 }
+
+const retroAjax = (filtro) => {
+    $.ajax({
+        url: 'https://retroapi-daw.herokuapp.com/api/v1/' + filtro,
+        type: 'GET',
+        dataType: 'json',
+        success: function (json) {
+            muestraEmulador(json);
+        },
+        error: function (jqXHR, status, error) {
+            muestraError( jqXHR.status + ' - ' + jqXHR.responseText);
+        }
+    });
+}
+
+const muestraError = (error) => {
+    
+}
+
+const muestraEmulador = (json) => {
+    let salida = '<div class="nk-gap-2"></div><div class="row vertical-gap text-white"><div class="col-lg-12"><div class="nk-box-2 bg-dark-2"><h4>Resultado</h4>';
+    for (let device of json.devices) {
+        salida += JSON.stringify(device) + '</div></div>';
+    }
+    $('#mostrar').html(salida);
+}
+
+document.getElementById("buscar-dispositivo").addEventListener("click", crearPeticionDispositivo);
