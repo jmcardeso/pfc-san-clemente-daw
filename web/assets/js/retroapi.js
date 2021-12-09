c5a36383e08f1aca0a190639e98ffd2cf90cab30
Juan M. Cardeso <a18juancg@iessanclemente.net>
2021-12-09 21:35:29 +0100

    Perfilando la búsqueda en la web

--- web/assets/js/retroapi.js
+++ web/assets/js/retroapi.js
@@ -2,33 +2,73 @@ const crearPeticionDispositivo = () => {
     if ($('#txtNombre').val() != "") {
         let url = 'devices?name=' + $('#txtNombre').val();
         retroAjax(url);
-    } else alert("No hay nombre");  
+    } else alert("No hay nombre");
 }
 
-const retroAjax = (filtro) => {
+const retroAjaxListaDispositivos = () => {
     $.ajax({
-        url: 'https://retroapi-daw.herokuapp.com/api/v1/' + filtro,
+        url: 'https://retroapi-daw.herokuapp.com/api/v1/devices?all=1&lang=es',
         type: 'GET',
         dataType: 'json',
         success: function (json) {
-            muestraEmulador(json);
+            let lista = llenaListaSugerencias(json.devices);
+
+            $('#txtNombre').autocomplete({
+                label: 'label',
+                value: 'value',
+                source: lista,
+                onSelectItem: onSelectItem,
+                treshold: 2,
+                highlightClass: 'text-danger'
+            });
         },
         error: function (jqXHR, status, error) {
-            muestraError( jqXHR.status + ' - ' + jqXHR.responseText);
+            muestraError(jqXHR.status + ' - ' + jqXHR.responseText);
+        }
+    });
+}
+
+const retroAjax = (nombreDispositivo) => {
+    $.ajax({
+        url: 'https://retroapi-daw.herokuapp.com/api/v1/devices?name=' + nombreDispositivo,
+        type: 'GET',
+        dataType: 'json',
+        success: function (json) {
+            muestraEmulador(json.devices, nombreDispositivo);
+        },
+        error: function (jqXHR, status, error) {
+            muestraError(jqXHR.status + ' - ' + jqXHR.responseText);
         }
     });
 }
 
 const muestraError = (error) => {
-    
+
 }
 
-const muestraEmulador = (json) => {
+const muestraEmulador = (dispositivos, nombreDispositivo) => {
+
     let salida = '<div class="nk-gap-2"></div><div class="row vertical-gap text-white"><div class="col-lg-12"><div class="nk-box-2 bg-dark-2"><h4>Resultado</h4>';
-    for (let device of json.devices) {
-        salida += JSON.stringify(device) + '</div></div>';
-    }
+    let dispositivo = dispositivos.find(element => element.name = nombreDispositivo);
+
+    salida += JSON.stringify(dispositivo) + '</div></div>';
+
     $('#mostrar').html(salida);
 }
 
-document.getElementById("buscar-dispositivo").addEventListener("click", crearPeticionDispositivo);
+const llenaListaSugerencias = (devices) => {
+    let datos = new Array();
+    let contador = 0;
+
+    for (let device of devices) {
+        datos.push(JSON.parse('{"label" : "' + device.name + '",  "value" : ' + ++contador + '}'));
+    }
+
+    return datos;
+}
+
+const onSelectItem = (seleccion) => {
+    retroAjax(seleccion.label);
+}
+
+retroAjaxListaDispositivos();
