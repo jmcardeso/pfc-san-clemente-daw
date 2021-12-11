13baa17015aa1ca8ac4f103f79a63a851d3ef85b
Juan M. Cardeso <a18juancg@iessanclemente.net>
2021-12-11 13:20:02 +0100

    Incluyendo imágenes en dispositivo

--- web/assets/js/retroapi.js
+++ web/assets/js/retroapi.js
@@ -46,14 +46,15 @@ const muestraError = (error) => {
                     <em>${error}</em>
                 </div>`;
     $('#mostrar').html(salida);
+    $('#mostrar').parent().removeClass('col-lg-8').addClass('col-lg-12');
+    $('#ficha').attr('hidden', false);
 }
 
 const muestraEmulador = (nombreDispositivo) => {
     try {
         let salida = `<div class="nk-gap-2"></div>
                         <div class="row vertical-gap text-white">
-                            <div class="col-lg-8">
-                                <div class="nk-box-2 bg-dark-2">`
+                            <div class="nk-box-2 bg-dark-2">`
 
         let dispositivo = dispositivos.find(element => element.name == nombreDispositivo);
         if (dispositivo == undefined) {
@@ -67,18 +68,44 @@ const muestraEmulador = (nombreDispositivo) => {
                     <h3>No encontrado</h3>
                     <em>No se ha encontrado ningúnn dispositivo con ese nombre.</em>
                 </div>`;
+            $('#mostrar').parent().removeClass('col-lg-8').addClass('col-lg-12');
+            $('#sidebar').attr('hidden', true);
         } else {
             salida += `<h3 class="text-main-1">${dispositivo.name}</h3>`;
-            if (dispositivo.description.length > 0) salida += `<p class="text-white">${dispositivo.description[0].content}</p>`;
-            salida += `<p class="text-secondary">CPU: <span class="text-white">${typeof dispositivo.cpu != 'undefined' ? dispositivo.cpu : ""}</span></p>
 
+            if (dispositivo.image.length > 0) {
+                salida += '<div class="nk-post-img">';
+                salida += `<img src="${dispositivo.image[0]}" alt="${dispositivo.name}">`
+                salida += `</div>
+                            <div class="nk-gap-2"></div>`;
+            }
+
+            if (dispositivo.description.length > 0) salida += `<p class="text-white">${dispositivo.description[0].content}</p>`;
+            salida += `<p class="text-secondary">Fabricante: <span class="text-white">${typeof dispositivo.manufacturer != 'undefined' ? dispositivo.manufacturer : ""}</span></p>`;
+            salida += `<p class="text-secondary">Tipo: <span class="text-white">${typeof dispositivo.type != 'undefined' ? dispositivo.type : ""}</span></p>`;
+            salida += `<p class="text-secondary">Año de lanzamiento: <span class="text-white">${typeof dispositivo.year != 'undefined' ? dispositivo.year : ""}</span></p>`;
+            salida += `<p class="text-secondary">CPU: <span class="text-white">${typeof dispositivo.cpu != 'undefined' ? dispositivo.cpu : ""}</span></p>`;
+            salida += `<p class="text-secondary">Memoria: <span class="text-white">${typeof dispositivo.memory != 'undefined' ? dispositivo.memory : ""}</span></p>`;
+            
+            if (dispositivo.gamepad != undefined) {
+                salida += '<div class="col-sm-6 nk-post-img" style="padding-left: 0;">';
+                salida += '<p class="text-secondary">Mando:</p>';
+                salida += `<img src="${dispositivo.gamepad}" alt="${dispositivo.name}">`
+                salida += `</div>
+                            <div class="nk-gap-2"></div>`;
+            }
+
+            salida += `
             
             
-            
-            </div></div>`;
+            <p class="text-secondary"><small>Fuente: Wikipedia</small></p>
+            </div>`;
+            $('#sidebar').attr('hidden', false);
+            $('#mostrar').parent().removeClass('col-lg-12').addClass('col-lg-8');
         }
 
         $('#mostrar').html(salida);
+        $('#ficha').attr('hidden', false);
     } catch (error) {
         muestraError("Se ha producido un error.");
     }
